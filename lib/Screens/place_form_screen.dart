dda45750b70c291ceb6dcebb295949a81b5ab157
Douglas <doug.balbino@hotmail.com>
2025-10-02 20:05:57 -0300

    commit inicial com a estrutura inicial do projeto

---
+++ lib/Screens/place_form_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PlaceFormScreen extends StatefulWidget {
+  const PlaceFormScreen({Key? key}) : super(key: key);
+
+  @override
+  State<PlaceFormScreen> createState() => _PlaceFormScreenState();
+}
+
+class _PlaceFormScreenState extends State<PlaceFormScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/utils/app_routes.dart
@@ -0,0 +1,3 @@
+class AppRoutes {
+  static const String placeform = '/place-form';
+}
